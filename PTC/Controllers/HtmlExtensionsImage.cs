0993bf8a9dfa2fd878b6c3a3d6f65dba1ca354ec
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-30 09:37:48 +0200

    Working my way through Module 2.

---
+++ PTC/Controllers/HtmlExtensionsImage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace PTC
+{
+    public static class HtmlExtensionsImage
+    {
+        public static MvcHtmlString Image(
+            this HtmlHelper htmlHelper,
+            string src,
+            string altText,
+            string cssClass,
+            string name)
+        {
+
+           TagBuilder tb = new TagBuilder("img");
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                name = TagBuilder.CreateSanitizedId(name);
+                tb.GenerateId(name);
+                tb.MergeAttribute("name", name);
+            }
+
+
+            tb.MergeAttribute("src", src);
+            tb.MergeAttribute("alt", altText);
+
+            if (!string.IsNullOrWhiteSpace(cssClass))
+            {
+
+            tb.AddCssClass(cssClass);
+
+            }
+
+            // HTML Encode the String
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
